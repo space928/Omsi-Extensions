602d0d08e88a8786ec050aa8b32abea26cba4670
Adam Mathieson <Adam@genav.ch>
2022-04-18 00:51:57 +0200

    + Start of RV  + Add Tickets to tester

--- OmsiHook/OmsiRoadVehicle.cs
+++ OmsiHook/OmsiRoadVehicle.cs
@@ -4,5 +4,17 @@
     {
         internal OmsiRoadVehicle(Memory omsiMemory, int baseAddress) : base(omsiMemory, baseAddress) { }
         internal OmsiRoadVehicle() : base() { }
+
+        /* TODO:
+        public OmsiScriptVarIndizes ScriptVarIndizes
+        {
+            get => Memory.ReadMemory<OmsiScriptVarIndizes>(Address + 0x2b8);
+            set => Memory.WriteMemory(Address + 0x2b8, value);
+        }*/
+        public bool Show_Dialogue
+        {
+            get => Memory.ReadMemory<bool>(Address + 0x500);
+            set => Memory.WriteMemory(Address + 0x500, value);
+        }
     }
 }
