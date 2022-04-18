82be054ff92a80e2325370a56129b00c7980abdc
Thomas Mathieson <thomas.mathieson02@gmail.com>
2022-04-19 00:37:53 +0100

    Pain and suffering... The calling convention is wrong because nobody in there right mind would use borland fastcall.

--- OmsiHookPlugin/OmsiHookPlugin.cs
+++ OmsiHookPlugin/OmsiHookPlugin.cs
@@ -4,12 +4,32 @@ using System.IO;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using OmsiHook;
+using System.Security;
+using System.Diagnostics;
 
 namespace OmsiHookPlugin
 {
     public class OmsiHookPlugin
     {
         private static OmsiHook.OmsiHook hook;
+        private static bool spawned = false;
+
+        private static readonly byte[] CALL_CONVENTION_FIXUP = { 0x5B, 0x5F, 0x58, 0x5A, 0x59, 0x53, 0xFF, 0xD7, 0xBF, 0xFF, 0xFF, 0xFF, 0xFF, 0xC3 };
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        delegate int CallConventionFixup(int address, int progMan, int vehList, int _RoadVehicleTypes, bool onlyvehlist, bool CS,
+              float TTtime, bool situationload, bool dialog, bool setdriver, bool thread,
+              int kennzeichen_index, bool initcall, int startday, byte trainbuilddir, bool reverse,
+              int grouphof, int typ, int tour, int line, int farbschema, bool Scheduled,
+              bool AIRoadVehicle, bool kennzeichen_random, bool farbschema_random, int filename);
+        private static CallConventionFixup callConventionFixup;
+        private static void CreateCallConventionFixup()
+        {
+            var ptr = Marshal.AllocHGlobal(CALL_CONVENTION_FIXUP.Length);
+            Marshal.Copy(CALL_CONVENTION_FIXUP, 0, ptr, CALL_CONVENTION_FIXUP.Length);
+            VirtualProtectEx(Process.GetCurrentProcess().Handle, ptr,
+                (UIntPtr)CALL_CONVENTION_FIXUP.Length, 0x40, out uint _);
+            callConventionFixup = Marshal.GetDelegateForFunctionPointer<CallConventionFixup>(ptr);
+        }
 
         private static void Log(string msg) => File.AppendAllText("omsiHookPluginLog.txt", $"[{DateTime.Now:dd/MM/yy HH:mm:ss:ff}] {msg}\n");
 
@@ -20,6 +40,23 @@ namespace OmsiHookPlugin
                 .DynamicInvoke(hook.ReadMemory(0x008591e8), 0);
         }
 
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        delegate int MakeVehicle(int progMan, int vehList, int _RoadVehicleTypes, bool onlyvehlist, bool CS,
+              float TTtime, bool situationload, bool dialog, bool setdriver, bool thread,
+              int kennzeichen_index, bool initcall, int startday, byte trainbuilddir, bool reverse,
+              int grouphof, int typ, int tour, int line, int farbschema, bool Scheduled,
+              bool AIRoadVehicle, bool kennzeichen_random, bool farbschema_random, int filename);
+        public static void SpawnBusTest()
+        {
+           // Marshal.GetDelegateForFunctionPointer<MakeVehicle>(new IntPtr(0x0070a250))
+            callConventionFixup(0x0070a250, hook.ReadMemory(0x00862f28), hook.ReadMemory(0x0086150C), 
+                hook.ReadMemory(0x008615A8), false, false,
+              0, false, false, true, false,
+              -1, true, 0, (byte)3, false,
+              0, 0, 0, 0, 0, false,
+              true, true, true, 0);
+        }
+
         [UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvCdecl) }, EntryPoint = nameof(PluginStart))]
         public static void PluginStart(IntPtr aOwner)
         {
@@ -27,8 +64,14 @@ namespace OmsiHookPlugin
             Log("Loading OmsiHook...");
             hook = new();
             hook.AttachToOMSI();
-            Log("Attempting dodginess...");
-            TestInvoke();
+            Log("Creating call convention fixer...");
+            try
+            {
+                CreateCallConventionFixup();
+            } catch (Exception ex)
+            {
+                Log(ex.ToString());
+            }
 
             Log("Didn't crash!");
         }
@@ -48,7 +91,27 @@ namespace OmsiHookPlugin
         [UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvCdecl) }, EntryPoint = nameof(AccessVariable))]
         public static void AccessVariable(ushort variableIndex, [C99Type("float*")] IntPtr value, [C99Type("__crt_bool*")] IntPtr writeValue)
         {
-            Log("AccessVariable()");
+            var val = Marshal.PtrToStructure<float>(value);
+            if (val > 0.9 && !spawned)
+            {
+                spawned = true;
+                Log("Spawning bus!");
+                try
+                {
+                    SpawnBusTest();
+                } catch (Exception e)
+                {
+                    Log("Uh oh:");
+                    Log(e.Message);
+                    Log(e.ToString());
+                }
+                
+                Log("The bus might have spawned!");
+            } else
+            {
+                if (val < 0.1)
+                    spawned = false;
+            }
         }
 
         [UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvCdecl) })]
@@ -62,5 +125,12 @@ namespace OmsiHookPlugin
         {
             
         }
+
+        [SuppressUnmanagedCodeSecurity]
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate int AssemblyAddFunction(int x, int y);
+
+        [DllImport("kernel32.dll")]
+        private static extern bool VirtualProtectEx(IntPtr hProcess, IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
     }
 }
