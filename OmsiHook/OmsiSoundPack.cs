ed24381b5d292d93437cf975fee81b2a764e0587
Adam Mathieson <Adam@genav.ch>
2022-04-18 13:27:44 +0200

    + OmsiSoundPack

---
+++ OmsiHook/OmsiSoundPack.cs
@@ -0,0 +1,80 @@
+﻿namespace OmsiHook
+{
+    public class OmsiSoundPack : OmsiObject
+    {
+        internal OmsiSoundPack(Memory omsiMemory, int baseAddress) : base(omsiMemory, baseAddress) { }
+        internal OmsiSoundPack() : base() { }
+
+        /*public DirectSound8 Device
+        {
+            get => Memory.ReadMemory<DirectSound8>(Address + 0x4);
+            set => Memory.WriteMemory(Address + 0x4, value);
+        }*/
+        public string FileName
+        {
+            get => Memory.ReadMemoryString(Memory.ReadMemory<int>(Address + 0x8));
+            set => Memory.WriteMemory(Address + 0x8, value);
+        }
+        public bool AI
+        {
+            get => Memory.ReadMemory<bool>(Address + 0xc);
+            set => Memory.WriteMemory(Address + 0xc, value);
+        }
+        public string Path
+        {
+            get => Memory.ReadMemoryString(Memory.ReadMemory<int>(Address + 0x10));
+            set => Memory.WriteMemory(Address + 0x10, value);
+        }
+        public int SoundCount
+        {
+            get => Memory.ReadMemory<int>(Address + 0x14);
+            set => Memory.WriteMemory(Address + 0x14, value);
+        }
+        public OmsiSound[] Sounds
+        {
+            get => Memory.ReadMemoryObjArray<OmsiSound>(Address + 0x18);
+        }
+        public float RefRange
+        {
+            get => Memory.ReadMemory<float>(Address + 0x1c);
+            set => Memory.WriteMemory(Address + 0x1c, value);
+        }
+        /* TODO:
+        public OmsiSingleVarsPnter[] Vars
+        {
+            get => Memory.ReadMemory<OmsiSingleVarsPnter>(Address + 0x20);
+            set => Memory.WriteMemory(Address + 0x20, value);
+        }*/
+        public D3DMatrix KoordSystem
+        {
+            get => Memory.ReadMemory<D3DMatrix>(Memory.ReadMemory<int>(Address + 0x24));
+            set => Memory.WriteMemory(Memory.ReadMemory<int>(Address + 0x24), value);
+        }
+        public bool Loaded
+        {
+            get => Memory.ReadMemory<bool>(Address + 0x28);
+            set => Memory.WriteMemory(Address + 0x28, value);
+        }
+        public float Dist
+        {
+            get => Memory.ReadMemory<float>(Address + 0x2c);
+            set => Memory.WriteMemory(Address + 0x2c, value);
+        }
+        public string SoundIdent
+        {
+            get => Memory.ReadMemoryString(Memory.ReadMemory<int>(Address + 0x30), true);
+            set => Memory.WriteMemory(Address + 0x30, value);
+        }
+        public bool TooFar
+        {
+            get => Memory.ReadMemory<bool>(Address + 0x34);
+            set => Memory.WriteMemory(Address + 0x34, value);
+        }
+        public bool Stopped_TooFar
+        {
+            get => Memory.ReadMemory<bool>(Address + 0x35);
+            set => Memory.WriteMemory(Address + 0x35, value);
+        }
+
+    }
+}
