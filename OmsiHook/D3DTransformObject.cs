bcfc87817a242964b15570cda6cd5e1ffa3d4cf2
Adam Mathieson <Adam@genav.ch>
2022-05-04 00:08:38 +0200

    + D3DTransformObject

--- OmsiHook/D3DTransformObject.cs
+++ OmsiHook/D3DTransformObject.cs
@@ -4,5 +4,21 @@
     {
         internal D3DTransformObject(Memory omsiMemory, int baseAddress) : base(omsiMemory, baseAddress) { }
         public D3DTransformObject() : base() { }
+
+        public D3DVector VFDmax
+        {
+            get => Memory.ReadMemory<D3DVector>(Address + 0x8);
+            set => Memory.WriteMemory(Address + 0x8, value);
+        }
+        public D3DVector VFDmin
+        {
+            get => Memory.ReadMemory<D3DVector>(Address + 0x14);
+            set => Memory.WriteMemory(Address + 0x14, value);
+        }
+        public D3DMatrix Matrix
+        {
+            get => Memory.ReadMemory<D3DMatrix>(Address + 0x20);
+            set => Memory.WriteMemory(Address + 0x20, value);
+        }
     }
 }
