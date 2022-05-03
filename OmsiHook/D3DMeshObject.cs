f23301ca42ee22387a6528dbc3c72eb9ed852409
Adam Mathieson <Adam@genav.ch>
2022-05-04 00:06:16 +0200

    + D3DMeshObject

--- OmsiHook/D3DMeshObject.cs
+++ OmsiHook/D3DMeshObject.cs
@@ -4,5 +4,73 @@
     {
         internal D3DMeshObject(Memory omsiMemory, int baseAddress) : base(omsiMemory, baseAddress) { }
         public D3DMeshObject() : base() { }
+        
+        public D3DMatrix OriginMatrix
+        {
+            get => Memory.ReadMemory<D3DMatrix>(Address + 0x60);
+            set => Memory.WriteMemory(Address + 0x60, value);
+        }
+        /// <summary>
+        /// ! Warn ! Listed Data type: ID3DxMesh
+        /// </summary>
+        public int Mesh // ID3DxMesh I guess read in as a ptr?
+        {
+            get => Memory.ReadMemory<int>(Address + 0xa0);
+            set => Memory.WriteMemory(Address + 0xa0, value);
+        }
+        public D3DOBB OBB
+        {
+            get => Memory.ReadMemory<D3DOBB>(Address + 0xa4);
+            set => Memory.WriteMemory(Address + 0xa4, value);
+        }
+        /// <summary>
+        /// ! Warn ! Listed Data type: ID3DXBuffer
+        /// </summary>
+        public int D3DXBuffer_Adja // ID3DXBuffer I guess read in as a ptr?
+        {
+            get => Memory.ReadMemory<int>(Address + 0xa8);
+            set => Memory.WriteMemory(Address + 0xa8, value);
+        }
+        /// <summary>
+        /// ! Warn ! Listed Data type: ID3DXBuffer
+        /// </summary>
+        public int D3DXBuffer_Mtrl // ID3DXBuffer I guess read in as a ptr?
+        {
+            get => Memory.ReadMemory<int>(Address + 0xac);
+            set => Memory.WriteMemory(Address + 0xac, value);
+        }
+        /// <summary>
+        /// ! Warn ! Listed Data type: ID3DXBuffer
+        /// </summary>
+        public int D3DXBuffer_Eff // ID3DXBuffer I guess read in as a ptr?
+        {
+            get => Memory.ReadMemory<int>(Address + 0xb0);
+            set => Memory.WriteMemory(Address + 0xb0, value);
+        }
+        public OmsiMaterialProp[] Materials
+        {
+            get => Memory.MarshalStructs<OmsiMaterialProp, OmsiMaterialPropInternal>(
+                Memory.ReadMemoryStructArray<OmsiMaterialPropInternal>(Address + 0xec));
+        }
+        /*
+        public string[] MaterialNames
+        {
+            get => Memory.ReadMemoryObjArray<string>(Address + 0xf0);
+        }*/
+        public bool UseCharMatrix
+        {
+            get => Memory.ReadMemory<bool>(Address + 0xf4);
+            set => Memory.WriteMemory(Address + 0xf4, value);
+        }
+        public D3DMatrix CharMatrix
+        {
+            get => Memory.ReadMemory<D3DMatrix>(Address + 0xf5);
+            set => Memory.WriteMemory(Address + 0xf5, value);
+        }
+        public D3DMatrix CharMatrixInv
+        {
+            get => Memory.ReadMemory<D3DMatrix>(Address + 0x135);
+            set => Memory.WriteMemory(Address + 0x135, value);
+        }
     }
 }
