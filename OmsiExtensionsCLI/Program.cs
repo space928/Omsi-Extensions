37d36b0f715ebc16ea5aaed60d25e7396f213737
Thomas Mathieson <thomas.mathieson02@gmail.com>
2022-05-03 18:18:31 +0100

    * Continued experiments

--- OmsiExtensionsCLI/Program.cs
+++ OmsiExtensionsCLI/Program.cs
@@ -30,9 +30,10 @@ namespace OmsiExtensionsCLI
                 Console.WriteLine($"Read data: ax:{posa._30:F3}   ay:{posa._31:F3}   az:{posa._32:F3}".PadRight(Console.WindowWidth-1));
 
                 Console.WriteLine($"Read data: map:{map.Name}   path:{map.Filename}   friendly:{map.FriendlyName}".PadRight(Console.WindowWidth-1));
+                Console.WriteLine($"{omsi.PlayerVehicle.PAI_LastBrake} {omsi.PlayerVehicle.Bremspedal}".PadRight(Console.WindowWidth - 1));
                 Console.WriteLine("".PadRight(Console.WindowWidth-1));
-                omsi.PlayerVehicle.Velocity = new D3DVector { x=0, y=0, z=5 };
-                var b = omsi.PlayerVehicle.PAI_LastBrake;
+                //omsi.PlayerVehicle.Velocity = new D3DVector { x=0, y=0, z=5 };
+                //omsi.PlayerVehicle.Bremspedal = 0;
 
                 Thread.Sleep(50);
             }
