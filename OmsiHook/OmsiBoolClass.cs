451fdef1695ea5ced8f9e8fcf8a084ba522a7696
Adam Mathieson <Adam@genav.ch>
2022-05-22 00:23:01 +0200

    + OmsiSound Completed for @sjain882  + OmsiBoolClass  + Required Structs

---
+++ OmsiHook/OmsiBoolClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmsiHook
+{
+    /// <summary>
+    /// Defines a set of conditions based on various data types.
+    /// </summary>
+    public class OmsiBoolClass : OmsiObject
+    { 
+        internal OmsiBoolClass(Memory omsiMemory, int baseAddress) : base(omsiMemory, baseAddress) { }
+        public OmsiBoolClass() : base() { }
+        
+        public OmsiBoolClassCondiBool[] CondisBool
+        {
+            get => Memory.ReadMemoryStructArray<OmsiBoolClassCondiBool>(Address + 0x4);
+        }
+        public OmsiBoolClassCondiInt[] CondisInt
+        {
+            get => Memory.ReadMemoryStructArray<OmsiBoolClassCondiInt>(Address + 0x8);
+        }
+        public OmsiBoolClassCondiFloat[] CondisFloat
+        {
+            get => Memory.ReadMemoryStructArray<OmsiBoolClassCondiFloat>(Address + 0xc);
+        }
+    }
+}
