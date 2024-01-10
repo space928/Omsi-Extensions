21fb0b9321641143369c669076d323548f2194a0
Adam Mathieson <adam@mathieson.dev>
2024-01-10 22:12:00 +0100

    + Initial Commit

---
+++ _OmsiHookExamples/ClickablePlaneDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using System.Reflection;
+using System.Threading;
+using OmsiHook;
+
+namespace ClickablePlaneDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("#=#=#=#=#=# OmsiExtensions Testing #=#=#=#=#=#");
+
+            OmsiHook.OmsiHook omsi = new();
+            omsi.AttachToOMSI().Wait();
+            Console.Clear();
+        }
+    }
+}
